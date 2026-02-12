6cf24b06864645d3539153606ea938c9c58e2c1f
vimaleshwar.kk <vimaleshwar.kk@juspay.in>
2026-02-12 12:42:40 +0530

    Add text-2.0+ compatibility-4

--- src/Streamly/Data/Serialize/Instances/Text.hs
+++ src/Streamly/Data/Serialize/Instances/Text.hs
@@ -28,7 +28,8 @@ data MyArray = MyArray ByteArray#
 #else
 
 import qualified Data.Text.Array as TArr (Array(..))
-#define LEN_TO_BYTES(l) (l * 2)
+#define T_ARR_CON TArr.Array
+#define LEN_TO_BYTES(l) l * 2
 
 #endif
 
